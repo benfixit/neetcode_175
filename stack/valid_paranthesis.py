ee990baf8dbe36ae7bec81e76638a65b0a6334ab
Chukwuemeka Inya <chukwuemekainya@Chukwuemekas-MacBook-Pro.local>
2022-07-17 19:03:29 +0200

    Create stack folder and solve valid parenthesis

---
+++ stack/valid_paranthesis.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = list()
+        map = {")": "(", "]": "[", "}": "{"}
+
+        for br in s:
+            if br == "(" or br == "[" or br == "{":
+                stack.append(br)
+            elif br in map and stack and stack[-1] == map[br]:
+                stack.pop()
+            else:
+                return False
+
+        return len(stack) == 0
+
+
+
+c = Solution()
+
+print(c.isValid("()"))
+print(c.isValid("()[]{}"))
+print(c.isValid("(]"))
